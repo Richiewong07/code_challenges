a49e0234d96a84d58fcc28bacd743ac27c928c9c
Richie Wong <richiewong07@yahoo.com>
2018-02-11 17:19:53 -0600

    updated

--- 3_fibonacci.js
+++ 3_fibonacci.js
@@ -1,8 +1,7 @@
 // 3. Fibonacci
+
 // Question: How do get nth Fibonacci number?
-//
-//
+
 // Answer: I create an array and start from iterate through.
-//
-//
+
 // Fibonacci series is one of the most popular interview question for beginners. so, you have to learn this one.
---
+++ 4_greatestCommonDivisor.js
@@ -0,0 +1,24 @@
+// 4. Greatest Common Divisor
+
+// Question: How would you find the greatest common divisor of two numbers?
+
+function greatestCommonDivisor(a, b) {
+  var divisor = 2;
+  var greatestDivisor = 1;
+
+  if (a < 2 || b < 2) {
+    return 1;
+  }
+
+  while (a >= divisor && b >= divisor) {
+    if (a % divisor == 0 && b % divisor == 0) {
+      greatestDivisor = divisor;
+    }
+    divisor++;
+  }
+  return greatestDivisor;
+}
+
+console.log(greatestCommonDivisor(14, 21));
+
+console.log(greatestCommonDivisor(69, 169));
